fe7d4c0322f5061ecb349b434d9cd3c539d058ee
SaroarShahan <saroar.shahan@gmail.com>
2024-12-20 00:47:07 +0600

    feat: complete highest score problem

---
+++ day-004/highest_score.py
@@ -0,0 +1,9 @@
+scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+highest_score = 0
+
+for score in scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
