86a3e9b24e2f77f258fb5906818f1ebbfb0db611
SaroarShahan <saroar.shahan@gmail.com>
2024-12-19 14:20:54 +0600

    feat: complete day 1.4 lession

---
+++ day-001/main-1.4.py
@@ -0,0 +1,14 @@
+# Write a program that switches the values stored in the variables a and b. 
+
+# Inputs
+a = input("a: ")
+b = input("b: ")
+
+# Solution
+temp = a
+a = b
+b = temp
+
+# Outputs
+print("a = ", a)
+print("b = ", b)
